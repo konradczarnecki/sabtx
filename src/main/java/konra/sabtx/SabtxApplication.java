8459cb277a4bc8112fc9ac0ba90e93770d256a4d
konra <konraddczarnecki@gmail.com>
2018-03-26 00:38:34 +0200

    Init v0.1

---
+++ src/main/java/konra/sabtx/SabtxApplication.java
@@ -0,0 +1,12 @@
+package konra.sabtx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabtxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabtxApplication.class, args);
+	}
+}
---
+++ src/test/java/konra/sabtx/SabtxApplicationTests.java
@@ -0,0 +1,16 @@
+package konra.sabtx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SabtxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
